3a5256e8b21f569efa4e29e2a852844306b07355
ashleyrabanales <arabana2@students.kennesaw.edu>
2022-01-28 17:02:00 -0700

    1/28

--- data/child.py
+++ data/child.py
@@ -2,18 +2,24 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-from plotnine import *
+import matplotlib.pyplot as plt
+from statsmodels.graphics.mosaicplot import mosaic
+from itertools import product
 #%%
-
-child = pd.read_csv("/Users/ashleyrabanales/Desktop/STAT 4210 - Regression/Data Sets/.FC2019v1.csv.icloud")
-
+#https://medium.com/analytics-vidhya/hello-everyone-4f9400e008dc
+child = pd.read_csv("/Users/ashleyrabanales/Desktop/STAT 4210 - Regression/Data Sets/FC2019v1.csv")
+#%%
 child.describe(exclude=[np.number])  
-
 child.describe()
 
+
 # Drop incomplete values in race 
+#R-m code into python
 child = child[!child$race_ethnicity=="",]
 child = child[complete.cases(child$race_ethnicity),]
 
 #creating a new vari for race/enthnicity 
 
+
+
+chart.save("screenshots/altair_diamonds_price_density.png")
---
+++ data/intro.py
@@ -0,0 +1,38 @@
+
+# %%
+import numpy as py
+# %%
+type(bmi) # specific type of variable
+#float, loating point: a number that has both an integer and fractional part, separated by a point.
+#int -  integer: a number without a fractional part.
+#str - A string is Python's way to represent text
+# Bool -  boolean: a type to represent logical values. Can only be True or False (the capitalization is important!).
+
+#python list
+fam = [1.73, 1.68, 1.72, 1.89]
+
+#adding strings to name
+fam2= [["a", 1.73],
+        ["b", 1.68],
+        ["e", 1.72], 
+        ["w", 1.89]]
+print(fam2)
+
+#or
+#%%
+a = 1.73,
+b = 1.68
+e = 1.72,
+w = 1.89
+#%%
+fam4 = ["ash", a,
+        "bob", b,
+        "ellie", e,
+        "will", w]
+
+# %%
+
+#Subsetting List
+#%%
+
+#%%
